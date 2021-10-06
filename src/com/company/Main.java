2b65e6bdeb509fc496b8ac7845d9107efc8773b5
MGreco2112 <mgreco2112@tutanota.com>
2021-10-06 14:43:48 -0400

    Plan of attack outlined

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,67 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    /*
+	    Uno: Plan of Attack!
+
+	    Required Classes:
+
+	    Card Package:
+	    Card
+	    Deck
+
+	    Utility Package:
+	    GetString
+	    GetInt
+
+	    Game Package:
+	    Player
+	    Table
+
+
+	    Class Details:
+
+	    Card:
+	        String Color
+	        int value
+	        boolean isSkip
+	        boolean isReverse
+	        boolean isWild
+	        boolean isDraw
+
+	        constructor sets each of above
+
+	        determinePowers()
+	            if value == regular card: return;
+	            else: set proper boolean(s) to true;
+
+	    Deck:
+	        List<Card> cards
+
+	        constructor builds individual cards and adds them to cards collection
+
+	        draw()
+
+	        shuffle()
+	            shuffles cards collection
+
+	    Player:
+	        Hand hand
+	        int score
+
+	        playCard()
+
+	        draw(int number of cards)
+
+	    Table:
+	        List<Player> players
+
+	        round() loops turn
+
+	        turn() runs logic for each turn
+
+	        determineRotation() iterates through players in either direction depending on reverse cards in play
+
+        */
     }
 }
