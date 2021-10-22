c07deecde924b918849b3a6c831ce7eb620ee3e3
Matthew Greco <mgreco2112@tutanota.com>
2021-10-21 20:41:34 -0400

    created outlines for Actpr and Bot

--- src/game/Actor.java
+++ src/game/Actor.java
@@ -1,4 +1,8 @@
 package game;
 
 public interface Actor {
+    /*
+    Create an interface that contains all the external methods of Player
+    Implement this with Player and Bot
+     */
 }
--- src/game/Bot.java
+++ src/game/Bot.java
@@ -1,4 +1,14 @@
 package game;
 
 public class Bot {
+
+    /*
+    Bot should
+        -Play and damaging cards as a priority (skip, draw x, reverse)
+        -Play and valid cards as a secondary
+        -Play Wilds only if unable to play other cards
+        -If unable to play anything, draw
+     */
+
+
 }
